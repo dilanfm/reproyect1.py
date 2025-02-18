7bca7923cd1328e4ad3158b7da1ac7f69afce2c2
dilanfm <dilanfernandezmora@gmail.com>
2025-02-17 18:48:02 -0600

    Create program.py
    este es mi primer commit :)

---
+++ program.py
@@ -0,0 +1,9 @@
+lista_anime = []
+
+lista_anime.append("Jujutsu Kaisen")
+lista_anime.append("Naruto Shippuden")
+lista_anime.append("Goku")
+lista_anime.append("Ranma")
+
+for i in range(len(lista_anime)):
+    print(str(i+1) + "." + lista_anime[i])
